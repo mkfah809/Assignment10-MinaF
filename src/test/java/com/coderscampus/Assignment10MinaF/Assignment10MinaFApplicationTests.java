c704b56931cd6eec9e80c3dc29475dd6bb3faea9
Mina K. Fahmy <minakaramm1@gmail.com>
2022-02-02 19:42:23 -0500

    Setup POJOs

---
+++ src/test/java/com/coderscampus/Assignment10MinaF/Assignment10MinaFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Assignment10MinaF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment10MinaFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
